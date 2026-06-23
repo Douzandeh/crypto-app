6f21d157446d4a1a05ddef2e349e474c1bedcd21
Hossein Douzandeh <douzandeh.k@gmail.com>
2026-06-23 11:24:29 +0330

    feat: Implementing the Pagination component and pagination of coin data Styling the Pagination component

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -1,21 +1,27 @@
 import { useEffect, useState } from "react";
 import { getCoinList } from "../../services/CryptoApi";
 import TableCoin from "../modules/TableCoin";
+import Pagination from "../modules/Pagination";
 
 function HomePage() {
   const [coins, setCoins] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
+    setIsLoading(true);
     const getData = async () => {
-      const res = await fetch(getCoinList());
+      const res = await fetch(getCoinList(page));
       const json = await res.json();
       setCoins(json);
+      setIsLoading(false);
     };
     getData();
-  }, []);
+  }, [page]);
   return (
     <div>
       <TableCoin coins={coins} />
+      <Pagination page={page} setPage={setPage} />
     </div>
   );
 }
